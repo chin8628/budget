0e5fbb710db5e586dec2bf02b6192a586d16bcdb
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2023-06-03 23:07:17 +0700

    add trpc

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,37 @@
 import express, { Express, Request, Response } from "express";
+import { z } from "zod";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
 const app: Express = express();
 const port = 3000;
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = inferAsyncReturnType<typeof createContext>;
+const t = initTRPC.context<Context>().create();
+const appRouter = t.router({
+  getUser: t.procedure.input(z.string()).query((opts) => {
+    opts.input;
+    return { id: opts.input, name: "Bilbo" };
+  }),
+});
+export type AppRouter = typeof appRouter;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, this is Express + TypeScript");
 });
 
 app.listen(port, () => {
-  console.log(`[Server]: I am running at https://localhost:${port}`);
+  console.log(`[Server]: I am running at http://localhost:${port}`);
 });
