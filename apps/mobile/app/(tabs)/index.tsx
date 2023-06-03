8c9875dfe796d82aded234134b0e90a6b80c0e04
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2023-06-03 23:07:45 +0700

    convert mobile project to typescript template

---
+++ apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import { trpc } from "../../api";
+
+export default function TabOneScreen() {
+  const userQuery = trpc.getUser.useQuery("test");
+
+  console.log(userQuery)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One [{userQuery.data?.name} {userQuery.data?.id}]</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,71 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { httpBatchLink } from "@trpc/client";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "../api";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  return (
+    <>
+      {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
+      {!loaded && <SplashScreen />}
+      {loaded && <RootLayoutNav />}
+    </>
+  );
+}
+
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "http://172.17.180.132:3000/trpc",
+    }),
+  ],
+});
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+          </Stack>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
